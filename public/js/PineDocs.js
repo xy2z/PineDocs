b0f22c085b690c67289024f1f52c0a42ca9f4d77
Mylloon <kennel.anri@tutanota.com>
2022-04-11 13:10:57 +0200

    Add LaTeX rendering

--- public/js/PineDocs.js
+++ public/js/PineDocs.js
@@ -104,6 +104,17 @@ $(function() {
 				hljs.highlightBlock(block)
 			})
 
+			// LaTeX
+			var script = document.createElement('script');
+			script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+			script.async = true;
+			document.head.appendChild(script);
+			window.MathJax = {
+				tex: {
+					inlineMath: [['$', '$']]
+				}
+			};
+
 		} else if (data.type == 'image') {
 			// Image.
 			var img = $('<img>').attr('src', 'data:image/gif;base64,' + data.content)
