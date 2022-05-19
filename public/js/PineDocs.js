0f95649fb0edfb123e3b0f187d0e6d7bae94fb3c
Mylloon <kennel.anri@tutanota.com>
2022-05-19 18:07:22 +0200

    Use typeset() for MathJax

--- public/js/PineDocs.js
+++ public/js/PineDocs.js
@@ -106,8 +106,7 @@ $(function() {
 
 			// LaTeX
 			var script = document.createElement('script');
-			script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
-			script.async = true;
+			script.append("MathJax.typeset();");
 			document.head.appendChild(script);
 
 		} else if (data.type == 'image') {
